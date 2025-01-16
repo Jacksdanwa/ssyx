4a327a3d388b6122086feaf964c37db0ef540b71
ASUS <123@mail.com>
2025-01-16 11:09:48 +0800

    1

--- common/service-util/src/main/java/com/atme/ssyx/common/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/atme/ssyx/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package com.atme.ssyx.common.exception;
 
+import com.atme.ssyx.common.result.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 //AOP面向切面编程（不改变代码而增加一个功能）
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail(null);
+    }
 }
---
+++ common/service-util/src/main/java/com/atme/ssyx/common/result/Result.java
@@ -0,0 +1,41 @@
+package com.atme.ssyx.common.result;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    //返回状态码
+    private Integer code;
+    //返回信息
+    private String message;
+    //返回数据
+    private T data;
+
+    //私有化构造器
+    private Result() {
+    }
+
+    public static<T> Result<T> build(T data, ResultCodeEnum resultCodeEnum){
+        //创建结果返回类
+        Result<T> result = new Result<>();
+        if (data != null) {
+            //返回值不等于空的情况下，返回data的值
+            result.setData(data);
+        }
+        //设置返回的状态码
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(resultCodeEnum.getMessage());
+        return result;
+    }
+
+    //成功的方法
+    public static<T> Result<T> ok(T data) {
+        return build(data, ResultCodeEnum.SUCCESS);
+    }
+
+    //失败的方法
+    public static<T> Result<T> fail(T data){
+       return build(data, ResultCodeEnum.FAIL);
+    }
+
+}

bb56fa6dcb931c0e5145d906ace62cf6d701a226
ASUS <123@mail.com>
2025-01-16 10:27:28 +0800

    1

---
+++ common/service-util/src/main/java/com/atme/ssyx/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.atme.ssyx.common.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+//AOP面向切面编程（不改变代码而增加一个功能）
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
